72360fc9f0438c101ce3816e11ad37a4af316b96
ArtV97 <arthurvianna@id.uff.br>
2021-08-15 23:38:02 -0300

    json update

--- p3.jl
+++ p3.jl
@@ -9,13 +9,9 @@ function delta_x_plot(delta_x, d_out::Dict)
     d_out["delta_x = $delta_x"] = Dict()
     N, x = Metodos.calc_nx(delta_x, PVC.L)
 
-    #y = Metodos.analytic_solution(N, x, PVC.fy)
-    #func = plot(x, y, marker=:circle, label="S. Analítica")
     d_out["delta_x = $delta_x"]["Solucao Analítica"] = Metodos.analytic_solution(N, x, PVC.fy)
     func = plot(x, d_out["delta_x = $delta_x"]["Solucao Analítica"], marker=:circle, label="S. Analítica")
 
-    #w = Metodos.dif_finitas(delta_x, PVC, N)
-    #plot!(x, w, marker=:circle, label="Dif. Finitas", xlabel="Posição da Barra", ylabel="Temperatura")
     d_out["delta_x = $delta_x"]["Diferenças Finitas"] = Metodos.dif_finitas(delta_x, PVC, N)
     plot!(x, d_out["delta_x = $delta_x"]["Diferenças Finitas"], marker=:circle, label="Dif. Finitas", xlabel="Posição da Barra", ylabel="Temperatura")
 
@@ -32,11 +28,7 @@ function main()
     delta_x_plot(0.25, d_out)
 
     fout = open("output.json", "w")
-    for item in sort(collect(d_out), rev=true)
-        JSON.print(fout, item, 2)
-    end
-    #JSON.print(fout, d_out, 2)
-    #JSON.print(fout, sort(collect(d_out), rev=true), 2)
+    JSON.print(fout, sort(d_out, rev=true), 2)
     close(fout)
 end
 
